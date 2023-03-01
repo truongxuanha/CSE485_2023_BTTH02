342cdbe622d534731810b1f1e8920c8758b3e282
tranlam-2002 <tranlam2782002@gamil.com>
2023-03-01 13:35:47 +0700

    btth2

---
+++ btth01/footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
+    <h4 class="text-center text-uppercase fw-bold"><strong>TLU's music garden</strong></h4>
+</footer>
+</body>
+
+</html>
---
+++ btth01/signUp.php
@@ -0,0 +1,72 @@
+<?php include "./header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music for Life</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style_signup.css">
+</head>
+<?php
+	if (isset($_POST["btn_submit"])) {
+		//lấy thông tin từ các form bằng phương thức POST
+		$username = $_POST["username"];
+		$password = $_POST["pass"];
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		//Kiểm tra điều kiện bắt buộc đối với các field không được bỏ trống
+		if ($username == "" || $password == "" || $name == "" || $email == "") {
+			echo "bạn vui lòng nhập đầy đủ thông tin";
+		}else{
+			$sql = "INSERT INTO users(username, password, fullname, email, createdate ) VALUES ( '$username', '$password', '$name', '$email', now())";
+			// thực thi câu $sql với biến conn lấy từ file connection.php
+			mysqli_query($conn,$sql);
+			echo "chúc mừng bạn đã đăng ký thành công";
+		}
+	}
+
+?>
+<main class="container mt-5 mb-5">
+<div class="d-flex justify-content-center h-100">
+<div class="card">
+<div class="card-body">
+	<form action="dang-ky.php" method="post">
+		<table>
+			<tr>
+			<h3><strong>Sign Up</strong> </h3>
+                        <div class="d-flex justify-content-end social_icon">
+                            <span><i class="fab fa-facebook-square"></i></span>
+                            <span><i class="fab fa-google-plus-square"></i></span>
+                            <span><i class="fab fa-twitter-square"></i></span>
+                        </div>
+			</tr>	
+			<tr>
+				<td>Username :</td>
+				<td><input type="text" id="username" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password :</td>
+				<td><input type="password" id="pass" name="pass"></td>
+			</tr>
+			<tr>
+				<td>Ho Ten :</td>
+				<td><input type="text" id="name" name="name"></td>
+			</tr>
+			<tr>
+				<td>Email :</td>
+				<td><input type="text" id="email" name="email"></td>
+			</tr>
+		</table>
+		<div class="form-group">
+            <input type="submit" value="Sign Up" class="btn float-end signup_btn">
+        </div>
+			
+	</form>
+</div>
+</div>
+</div>
+</main>
+<?php require "./footer.php" ?>
